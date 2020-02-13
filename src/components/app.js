164ba356134db9c96272542925f774614bc4f2d2
Maximilien Mao <contact@mmao.fr>
2020-02-13 18:08:41 +0100

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import './app.scss'
+
+export default class app extends Component {
+    render() {
+        return (
+            <div className='container mx-auto flex justify-center'>
+                <div className='text-2xl'>
+                  <h1>React Boilerplate</h1>
+                </div>
+            </div>
+        )
+    }
+}
